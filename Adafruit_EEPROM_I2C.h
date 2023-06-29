4a4f19388f9cf6164b1fc398d1c40ed5a5983018
Tobias Bildner <tobiasbildner@gmail.com>
2023-06-29 19:30:56 +0200

    .

--- Adafruit_EEPROM_I2C.h
+++ Adafruit_EEPROM_I2C.h
@@ -2,7 +2,7 @@
 /*!
  * @file Adafruit_EEPROM_I2C.h
  */
- /**************************************************************************/
+/**************************************************************************/
 #ifndef _ADAFRUIT_EEPROM_I2C_H_
 #define _ADAFRUIT_EEPROM_I2C_H_
 
@@ -22,66 +22,65 @@ using pic_i2c::SoftI2C;
  */
 class Adafruit_EEPROM_I2C {
 public:
-    Adafruit_EEPROM_I2C(void);
+  Adafruit_EEPROM_I2C(void);
 
-    bool begin(uint8_t addr = EEPROM_DEFAULT_ADDRESS, TwoWire* theWire = &Wire);
+  bool begin(uint8_t addr = EEPROM_DEFAULT_ADDRESS, TwoWire *theWire = &Wire);
+  bool write(uint16_t addr, uint8_t value);
+  uint8_t read(uint16_t addr);
+  bool write(uint16_t addr, uint8_t *buffer, uint16_t num);
+  bool read(uint16_t addr, uint8_t *buffer, uint16_t num);
 
-    virtual bool begin(SoftI2C* wire, uint8_t i2c_addr = EEPROM_DEFAULT_ADDRESS) {
-        this->i2c_dev = new Adafruit_I2CDeviceSoft(i2c_addr, wire);
-        this->_addr = i2c_addr;
-        return (this->i2c_dev->begin());
-    };
+  virtual bool begin(SoftI2C* wire, uint8_t i2c_addr = EEPROM_DEFAULT_ADDRESS) {
+      this->i2c_dev = new Adafruit_I2CDeviceSoft(i2c_addr, wire);
+      this->_addr = i2c_addr;
+      return (this->i2c_dev->begin());
+  };
 
-    bool write(uint16_t addr, uint8_t value);
-    uint8_t read(uint16_t addr);
-    bool write(uint16_t addr, uint8_t* buffer, uint16_t num);
-    bool read(uint16_t addr, uint8_t* buffer, uint16_t num);
-
-    /**************************************************************************/
-    /*!
-        @brief  Write any object to memory
-        @param addr
-                    The 16-bit address to write to in EEPROM memory
-        @param value  The templated object we will be writing
-        @returns    The number of bytes written
-    */
-    /**************************************************************************/
-    template <class T> uint16_t writeObject(uint16_t addr, const T& value) {
-        const byte* p = (const byte*)(const void*)&value;
-        uint16_t n;
-        for (n = 0; n < sizeof(value); n++) {
-            write(addr++, *p++);
-        }
-        return n;
+  /**************************************************************************/
+  /*!
+      @brief  Write any object to memory
+      @param addr
+                  The 16-bit address to write to in EEPROM memory
+      @param value  The templated object we will be writing
+      @returns    The number of bytes written
+  */
+  /**************************************************************************/
+  template <class T> uint16_t writeObject(uint16_t addr, const T &value) {
+    const byte *p = (const byte *)(const void *)&value;
+    uint16_t n;
+    for (n = 0; n < sizeof(value); n++) {
+      write(addr++, *p++);
     }
+    return n;
+  }
 
-    /**************************************************************************/
-    /*!
-        @brief Read any object from memory
-        @param addr
-                    The 16-bit address to write to in EEPROM memory
-        @param value  The address of the templated object we will be writing INTO
-        @returns    The number of bytes read
-    */
-    /**************************************************************************/
+  /**************************************************************************/
+  /*!
+      @brief Read any object from memory
+      @param addr
+                  The 16-bit address to write to in EEPROM memory
+      @param value  The address of the templated object we will be writing INTO
+      @returns    The number of bytes read
+  */
+  /**************************************************************************/
 
-    template <class T> uint16_t readObject(uint16_t addr, T& value) {
-        byte* p = (byte*)(void*)&value;
-        uint16_t n;
-        for (n = 0; n < sizeof(value); n++) {
-            *p++ = read(addr++);
-        }
-        return n;
+  template <class T> uint16_t readObject(uint16_t addr, T &value) {
+    byte *p = (byte *)(void *)&value;
+    uint16_t n;
+    for (n = 0; n < sizeof(value); n++) {
+      *p++ = read(addr++);
     }
+    return n;
+  }
 
     virtual Adafruit_I2CDevice* getI2CDevice() { return this->i2c_dev; };
     virtual uint8_t getI2CAddress() { return this->_addr; };
 
 protected:
-    /*! The internal I2C device for communication */
-    Adafruit_I2CDevice* i2c_dev = NULL;
-    /*! The I2C address, sometimes needed! */
-    uint8_t _addr = 0;
+  /*! The internal I2C device for communication */
+  Adafruit_I2CDevice *i2c_dev = NULL;
+  /*! The I2C address, sometimes needed! */
+  uint8_t _addr = 0;
 };
 
 #endif
